c1612207740df8889405855ff5efe4120ea730dd
Ben Christel <bchristel@pivotal.io>
2016-07-31 11:49:54 -0700

    Make `gulp watch` not run the tests
    - `ava --watch` is faster than re-running ava each time gulp watch is triggered.

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,21 @@ var gulp = require('gulp')
 var babel = require('gulp-babel')
 var ava = require('gulp-ava')
 var watch = require('gulp-sane-watch')
+var gutil = require('gulp-util')
 
 gulp.task('default', gulp.series(build, test))
 
 gulp.task('watch', function() {
   watch('src/**/*.js', function() {
-    gulp.series(build, test)()
+    build()
   })
 })
 
 function build() {
+  console.log('running build')
   return gulp.src('src/*.js')
     .pipe(babel({presets: ['es2015'], plugins: ['transform-runtime']}))
+    .on('error', gutil.log)
     .pipe(gulp.dest('.'))
 }
 
